8f36fef820662fe570071127ac66e7d13e5dbdc3
kristenmills <kristenmills@users.noreply.github.com>
2020-01-25 01:53:16 +0000

    Automated deployment: Sat Jan 25 01:53:16 UTC 2020 f35edc551af4f4d9de31ca9cb674845e4713d85b

---
+++ templates/src/pages/404.tsx.993e73ba.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{52:function(n,t,e){"use strict";e.r(t);e(0);var u=e(4);t.default=function(){return Object(u.b)("div",null,Object(u.b)("h1",null,"404 - Oh no's! We couldn't find that page :("))}}}]);
